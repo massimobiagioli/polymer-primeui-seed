a14cc3e3923d14d5d10e666901c5e2d79f3f5a70
massimobiagioli <biagiolimassimo@gmail.com>
2016-05-22 10:49:26 +0200

    binding datatable

---
+++ script/app.js
@@ -0,0 +1,44 @@
+var App = {};		
+App.data = document.createElement("seed-app-container");
+App.data.treeDemo = {
+    nodes: [
+        {
+            data: {name: 'Documents', size: '75kb', type: 'Folder'},
+            children: [
+                {
+                    data: {name: 'Work', size: '55kb', type: 'Folder'},
+                    children: [
+                        {
+                            data: {name: 'Expenses.doc', size: '30kb', type: 'Document'}
+                        },
+                        {
+                            data: {name: 'Resume.doc', size: '25kb', type: 'Resume'}
+                        }
+                    ]
+                },
+                {
+                    data: {name: 'Home', size: '20kb', type: 'Folder'},
+                    children: [
+                        {
+                            data: {name: 'Invoices', size: '20kb', type: 'Text'}
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            data: {name: 'Pictures', size: '150kb', type: 'Folder'},
+            children: [
+                {
+                    data: {name: 'barcelona.jpg', size: '90kb', type: 'Picture'}
+                },
+                {
+                    data: {name: 'primeui.png', size: '30kb', type: 'Picture'}
+                },
+                {
+                    data: {name: 'optimus.jpg', size: '30kb', type: 'Picture'}
+                }
+            ]
+        }
+    ]
+};
